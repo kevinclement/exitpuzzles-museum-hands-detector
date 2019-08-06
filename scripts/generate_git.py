b7e8fa7b41cf85522ab1a5b60b848631f5f3a5a0
Kevin Clement <kevin.clement@gmail.com>
2019-08-06 14:51:47 -0700

    add hash and date

--- scripts/generate_git.py
+++ scripts/generate_git.py
@@ -1,1 +1,8 @@
+import os
+git_hash = os.popen('git --no-pager describe --tags --always --dirty').read().strip()
+git_date = os.popen('git --no-pager show --date=format:"%Y-%m-%d %H:%M:%S" --format="%ad" --name-only').read().partition('\n')[0].strip()
+
 print("######### generate_git_header #############")
+
+print("hash: '{0}'".format(git_hash))
+print("date: '{0}'".format(git_date))
